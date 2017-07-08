4161f2d8987e507d33977374e18e1e10670c9be2
acalc <acalc@protonmail.com>
2017-07-07 20:47:18 -0500

    Change 'shrink' to 'shorten'. Improve the landing page.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const APP_URL = "https://furly.herokuapp.com/"
+
 const express = require("express")
 const app = express()
 
@@ -5,7 +7,16 @@ app.set("port", (process.env.PORT || 8080))
 
 app.get("/", function(req, res) {
 	delete require.cache[require.resolve("./script.js")]
-	res.send("THE MAIN PAGE")
+	res.send(`
+<!DOCTYPE html>
+<html>
+ <head><title>furly</title></head>
+ <body>
+  <p><b>Shorten URL:</b> <pre>${APP_URL}shorten/your-url-here</pre></p>
+  <p><b>Goto URL:</b> <pre>${APP_URL}shortened-url-key</pre></p>
+ </body>
+</html>
+`)
 })
 
 // NOTE: url regexp from here: https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url/3809435#3809435
@@ -14,7 +25,7 @@ app.get("/", function(req, res) {
 // NOTE: instead of naming a parameter the usual way by prepending a colon,
 // this is like a pure regexp way of getting params, but enclosing the
 // relevant portion within parens. it is captured & written to req.params
-app.get(/^\/shrink\/([-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,8}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]{0,})?)/, function(req, res) {
+app.get(/^\/shorten\/([-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,8}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]{0,})?)/, function(req, res) {
 	delete require.cache[require.resolve("./script.js")]
 	require("./script.js").shortenUrl(req.params[0], res)
 })
